15554165e423b2e44f42039000befb9263e1e8ea
MrWouterNL <mail@mrwouter.nl>
2020-01-15 21:54:29 +0100

    indentation

--- src/main/java/nl/mrwouter/fixturecreator/objects/parameter/stops/ValueDisplayFormat.java
+++ src/main/java/nl/mrwouter/fixturecreator/objects/parameter/stops/ValueDisplayFormat.java
@@ -2,12 +2,13 @@ package nl.mrwouter.fixturecreator.objects.parameter.stops;
 
 public enum ValueDisplayFormat {
 	DEGREES("D"), NO_VALUE_DISPLAYED("N"), ONE_DIGIT_AS_PERCENTAGE("1"), TWO_DIGITS_AS_PERCENTAGE("2");
-	
+
 	private String abbreviaton;
+
 	private ValueDisplayFormat(String abbreviaton) {
 		this.abbreviaton = abbreviaton;
 	}
-	
+
 	public String getAbbreviaton() {
 		return abbreviaton;
 	}
