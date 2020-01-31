19033ae40d6f4f2b100efcccdf000dfb9ed654bd
MrWouterNL <mail@mrwouter.nl>
2020-01-31 21:25:00 +0100

    Removed notes from comments

--- src/main/java/nl/mrwouter/fixturecreator/objects/parameter/stops/ParameterStop.java
+++ src/main/java/nl/mrwouter/fixturecreator/objects/parameter/stops/ParameterStop.java
@@ -47,9 +47,6 @@ public class ParameterStop {
 
 	@Override
 	public String toString() {
-		// (M 0:255),D,0,170,"Pan="
-		// ( 32: 32),N,"Color 2"
-
 		String degrees = vdf == ValueDisplayFormat.DEGREES
 				? getDegreeRange().getStart() + "," + getDegreeRange().getEnd() + ","
 				: "";
@@ -61,40 +58,4 @@ public class ParameterStop {
 	private String addZeros(String string) {
 		return String.format("%1$3s", string);
 	}
-
-	// General:
-	// Mouseable
-	// Wheel stop range
-	// Value Display Format (D, N, 1, 2)
-	// Parameter stop name
-
-	// When VDF = D:
-	// Min-max degree value
-
-	// Non-degree based (color, gobo, etc.) :
-	// Mouseable (M of ' ')
-	// Wheel stop range (0:44)
-	// Value Display Format (D, N, 1, 2)
-	// Parameter stop name
-
-	// E.g.: stop = ( 32: 32),N,"Color 2"
-	// non mousable parameter (= "fixed value") stop with range 32-32, N = no
-	// display (only showing text, which is color 2 in this case)
-
-	// Would it make sense to have multiple degree based parameter stops..? I guess
-	// not.
-	// Degree based
-	// Mouseable (M of ' ')
-	// Wheel stop range (0:255)
-	// Value Display Format (is D, duhh..)
-	// Min,max degree value
-	// Parameter stop name
-
-	// E.g.: stop = (M 0:255),D,0,170,"Pan="
-	// Mousable parameter stop from 0-255, in degrees, with degree range from 0 to
-	// 170 degr. and the name is 'Pan'
-
-	// When a parameter has only one wheel stop and it is mouseable, the text must
-	// end with an equals (=) sign.
-
 }
